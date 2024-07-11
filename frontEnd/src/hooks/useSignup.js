adcad5cbb2f495f60aea6d2a3bf861e3477823d8
Bilal Arshad <barshad172@gmail.com>
2024-07-11 17:08:35 +0500

    auth context and registration

--- frontEnd/src/hooks/useSignup.js
+++ frontEnd/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { authUser, setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -33,7 +35,14 @@ const useSignup = () => {
           gender,
         }),
       });
-      const data = await res.json();
+        const data = await res.json();
+        if (data.error) {
+            throw new Error(data.error);
+      }
+      //local storage
+      localStorage.setItem("chat-user", JSON.stringify(data))
+      //context
+      setAuthUser(data)
       console.log(data);
     } catch (error) {
       toast.error(error.message);
