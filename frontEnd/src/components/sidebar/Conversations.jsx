62ed9342e6415ca4e0a3898981b63075fb242879
Bilal Arshad <barshad172@gmail.com>
2024-07-19 13:15:39 +0500

    send message functionality

--- frontEnd/src/components/sidebar/Conversations.jsx
+++ frontEnd/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Conversation from './Conversation';
+import useGetConversations from '../../hooks/useGetConversations';
 
 const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log(conversations)
   return (
       <div className='py-2 flex flex-col over-flow-auto'>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
+      {
+        conversations.map((conversation, index) => (
+          <Conversation
+            key={conversation._id}
+            conversation={conversation}
+            lastIndex = {index === conversations.length - 1}
+          />
+        ))}
+      {loading ? <span className='loading loading-spinner'></span> : null}
       </div>
   )
 }
