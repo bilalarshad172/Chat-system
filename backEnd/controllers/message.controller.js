3229795473c282b3a2412368eec1ee4e08bf7dc3
Bilal Arshad <barshad172@gmail.com>
2024-07-30 12:10:42 +0500

    implementing socket.io

--- backEnd/controllers/message.controller.js
+++ backEnd/controllers/message.controller.js
@@ -1,61 +1,65 @@
-import Conversation from '../models/conversation.model.js';
-import Message from '../models/message.model.js'
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+import { getReceiverSocketId } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
-    try {
-        const { message } = req.body;
-        const { id:receiverId } = req.params;
-        const senderId = req.user._id;
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-        let conversation = await Conversation.findOne({
-        participants:{$all:[senderId , receiverId]}
-        })
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-        if (!conversation) {
-            conversation = await Conversation.create({
-            participants:[senderId, receiverId]
-            })
-        }
-
-        const newMessage = new Message({
-            senderId,
-            receiverId,
-            message,
-        })
-
-        if (newMessage) {
-            conversation.messages.push(newMessage._id);
-        }
-
-        res.status(201).json(newMessage);
-
-        // This will run one by one which is not good practice.
-        // await conversation.save();
-        // await newMessage.save();
-
-        await Promise.all([conversation.save(), newMessage.save()]); //this will run in parallel.
-
-    } catch (error) {
-        console.log("Error in message Controller", error.message)
-        res.status(500).json({ error: "Internal server error" })
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
     }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+    // This will run one by one which is not good practice.
+    // await conversation.save();
+    // await newMessage.save();
+    await Promise.all([conversation.save(), newMessage.save()]); //this will run in parallel.
+    //socket IO functionality
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      //io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in message Controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const getMessage = async (req, res) => {
-    try {
-        const { id: userToChat } = req.params;
-        const senderId = req.user._id;
+  try {
+    const { id: userToChat } = req.params;
+    const senderId = req.user._id;
 
-        const conversation = await Conversation.findOne({
-            participants: { $all: [senderId, userToChat] },
-        }).populate("messages");
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChat] },
+    }).populate("messages");
 
-        if (!conversation) return res.status(200).json([]);
-        const messages = conversation.messages;
+    if (!conversation) return res.status(200).json([]);
+    const messages = conversation.messages;
 
-        res.status(200).json(messages);
-    } catch (error) {
-     console.log("Error in getMessage Controller", error.message);
-     res.status(500).json({ error: "Internal server error" });
-    }
-}
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessage Controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backEnd/server.js
+++ backEnd/server.js
@@ -5,8 +5,9 @@ import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import connectToMongo from "./db/connectToMongo.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -21,7 +22,7 @@ app.use("/api/users", userRoutes);
 //     res.send("Hello world111")
 // })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongo();
     console.log(`server running on ${PORT}`)
 })
